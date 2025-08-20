ab1ca0b81de41c78ebc7adc165c484c1a0d27752
evamonica100 <eva.monica02@gmail.com>
2025-08-20 16:46:03 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -100,7 +100,7 @@ export default function LoginPage() {
           />
         </div>
         <h1 className="text-xl sm:text-2xl font-bold text-center mb-4 sm:mb-6 text-blue-800">
-          Login to Scaling Assessment Software
+          Login to Ultrafiltration Membrane Calculator Software
         </h1>
         
         <form onSubmit={handleSubmit} className="space-y-4">
